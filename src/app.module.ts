34659c4055e2cee59ca30167cdef0980243070b7
Bal1oon <ws32159@naver.com>
2024-07-06 00:05:15 +0900

    Create movies module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController], // app.module은 AppController와 AppService만 가짐
+  providers: [],
 })
 export class AppModule {}
