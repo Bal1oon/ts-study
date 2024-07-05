f308d8540dd952445af9d3b38e6b63a08f3579a0
Bal1oon <ws32159@naver.com>
2024-07-05 23:42:39 +0900

    Create DTO

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Controller('movies')   // url의 Entry Point를 컨트롤 함
 export class MoviesController {
@@ -12,22 +13,22 @@ export class MoviesController {
     }
 
     @Get("/:id")
-    getOne(@Param("id") movieId: string): Movie {
+    getOne(@Param("id") movieId: number): Movie {
         return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData) {
+    create(@Body() movieData: CreateMovieDto) {
         return this.moviesService.create(movieData);
     }
 
     @Delete(":id")
-    remove(@Param("id") movieId:string) {
+    remove(@Param("id") movieId:number) {
         return this.moviesService.deleteOne(movieId);
     }
 
     @Patch('/:id')  // Put은 모든 리소스를 업데이트. patch는 리소스의 일부분만 업데이트
-    patch(@Param('id') movieId: string, @Body() updateData) {
+    patch(@Param('id') movieId: number, @Body() updateData) {
         return this.moviesService.update(movieId, updateData)
     }
 }
