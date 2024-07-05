b1980bbc604f59df01ef44f1e3faf21c31ccb683
Bal1oon <ws32159@naver.com>
2024-07-05 22:20:57 +0900

    Update controller using decorators

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')   // url의 Entry Point를 컨트롤 함
 export class MoviesController {
@@ -7,14 +7,19 @@ export class MoviesController {
         return "This will return all movies";
     }
 
+    @Get("/search") // @Get("/:id") 보다 아래에 있으면 search를 id로 판단하기 때문에 위에 배치
+    search(@Query("year") searchingYear: string) {
+        return `We are searching for a movie made after: ${searchingYear}`;
+    }
+
     @Get("/:id")
     getOne(@Param("id") movieId: string) {
         return `This will return one movie with the id: ${movieId}`;
     }
 
     @Post()
-    create() {
-        return 'This will create a movie';
+    create(@Body() movieData) {
+        return movieData;
     }
 
     @Delete(":id")
@@ -23,7 +28,10 @@ export class MoviesController {
     }
 
     @Patch('/:id')  // Put은 모든 리소스를 업데이트. patch는 리소스의 일부분만 업데이트
-    path(@Param('id') movieId: string) {
-        return `This will patch a movie with the id: ${movieId}`;
+    path(@Param('id') movieId: string, @Body() updateData) {
+        return {
+            updateMovie: movieId,
+            ...updateData,
+        };
     }
 }
