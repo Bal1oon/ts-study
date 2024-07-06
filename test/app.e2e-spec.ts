ea9d33872f6120b1da8fbeb21b100a07545842d0
Bal1oon <ws32159@naver.com>
2024-07-06 16:04:38 +0900

    test: Test GET movies with e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,  // 데코레이터가 없는 속성은 제거함
+        forbidNonWhitelisted: true, // 화이트리스트에 존재하지 않는 데이터가 있을 시 HttpException. whitelist: true가 선행되어야 함
+        transform: true,  // transform은 사용자의 값을 실제 타입으로 변환시켜줌 -> url은 전부 string이기 때문에 기존에는 id를 string으로 받아 int 타입으로 변환시켰음
+      }));
     await app.init();
   });
 
@@ -33,10 +39,10 @@ describe('AppController (e2e)', () => {
     it('POST', () => {
       return request(app.getHttpServer())
         .post('/movies')
-        .send({ 
-          title: "Test", 
-          year: 2000, 
-          genres: ['test'] 
+        .send({
+          title: "Test",
+          year: 2000,
+          genres: ['test']
         })
         .expect(201);
     });
@@ -47,4 +53,19 @@ describe('AppController (e2e)', () => {
         .expect(404);
     })
   });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(200)
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer())
+        .get('/movies/999')
+        .expect(404)
+    })
+    it.todo("DELETE");
+    it.todo("PATCH");
+  })
 });
