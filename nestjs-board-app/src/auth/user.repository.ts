34fce9bfa8daeff595b19ae44401a262931f71b1
Bal1oon <ws32159@naver.com>
2024-08-28 17:06:47 +0900

    feat: 유저 이름 유니크화

--- nestjs-board-app/src/auth/user.repository.ts
+++ nestjs-board-app/src/auth/user.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { DataSource, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
@@ -14,7 +14,15 @@ export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
         const user = this.create({ username, password });
-
-        await this.save(user);
+        
+        try {
+            await this.save(user);    
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Existing username');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
     }
 }
