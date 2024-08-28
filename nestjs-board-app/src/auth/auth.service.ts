ad765ae9c6582653ef31009a1e707611efe99d0b
Bal1oon <ws32159@naver.com>
2024-08-28 16:11:29 +0900

    feat: 회원가입 기능 구현

--- nestjs-board-app/src/auth/auth.service.ts
+++ nestjs-board-app/src/auth/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
 @Injectable()
 export class AuthService {
     constructor(
         private readonly userRepository: UserRepository
     ) {}
+
+    async singUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.createUser(authCredentialsDto);
+    }
 }
