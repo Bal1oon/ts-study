f3036a95a1df244d758105bc69784cbb52205588
Bal1oon <ws32159@naver.com>
2024-08-28 11:10:27 +0900

    feat: 로컬 메모리 사용에서 데이터베이스로 전환
    - 기존 로컬 메모리 관련 코드 주석 처리 및 삭제
      1. entity 사용으로 인한 model 삭제 (status는 필요)
      2. 불필요 경로 삭제

--- nestjs-board-app/src/boards/boards.service.ts
+++ nestjs-board-app/src/boards/boards.service.ts
@@ -1,47 +1,45 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid';
 import { CreaateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
-    private boards: Board[] = [];    // private을 사용하지 않으면 다른 컴포넌트에서 배열값을 수정할 수 있기 때문에 차단하기 위해 private 사용
+    // getAllBoards(): Board[] {   // return 값의 타입을 정의하기 위해 :Board[] 를 넣어줌
+    //     return this.boards;
+    // }
 
-    getAllBoards(): Board[] {   // return 값의 타입을 정의하기 위해 :Board[] 를 넣어줌
-        return this.boards;
-    }
+    // createBoard(createBoardDto: CreaateBoardDto) {
+    //     const { title, description } = createBoardDto;
+    //     const board: Board = {
+    //         id: uuid(),     // 로컬 메모리로 데이터를 저장하고 있어 uuid 임시 사용
+    //         title,          // JS에서는 title: title 처럼 key, value의 이름이 같으면 간소화할 수 있음
+    //         description,
+    //         status: BoardStatus.PUBLIC
+    //     }
 
-    createBoard(createBoardDto: CreaateBoardDto) {
-        const { title, description } = createBoardDto;
-        const board: Board = {
-            id: uuid(),     // 로컬 메모리로 데이터를 저장하고 있어 uuid 임시 사용
-            title,          // JS에서는 title: title 처럼 key, value의 이름이 같으면 간소화할 수 있음
-            description,
-            status: BoardStatus.PUBLIC
-        }
+    //     this.boards.push(board);
+    //     return board
+    // }
 
-        this.boards.push(board);
-        return board
-    }
+    // getBoardById(id: string): Board {
+    //     const found = this.boards.find((board) => board.id === id);
 
-    getBoardById(id: string): Board {
-        const found = this.boards.find((board) => board.id === id);
+    //     if (!found) {
+    //         throw new NotFoundException(`Can't find Board with id ${id}`);
+    //     }
 
-        if (!found) {
-            throw new NotFoundException(`Can't find Board with id ${id}`);
-        }
+    //     return found;
+    // }
 
-        return found;
-    }
+    // deleteBoard(id: string): void {
+    //     const found = this.getBoardById(id);
+    //     this.boards = this.boards.filter((board) => board.id !== found.id);
+    // }
 
-    deleteBoard(id: string): void {
-        const found = this.getBoardById(id);
-        this.boards = this.boards.filter((board) => board.id !== found.id);
-    }
-
-    updateBoardStatus(id: string, status: BoardStatus): Board {
-        const board = this.getBoardById(id);
-        board.status = status;
-        return board;
-    }
+    // updateBoardStatus(id: string, status: BoardStatus): Board {
+    //     const board = this.getBoardById(id);
+    //     board.status = status;
+    //     return board;
+    // }
 }
