47c6652295162ae9341def844f4b07d3c2cae920
Bal1oon <ws32159@naver.com>
2024-08-29 12:53:08 +0900

    feat: 토큰 인증 후 유저 정보 가져오기
    - Passport, JWT 사용

--- nestjs-board-app/src/auth/auth.module.ts
+++ nestjs-board-app/src/auth/auth.module.ts
@@ -7,6 +7,7 @@ import { User } from './user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConfig } from './configs/jwt.config';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -15,6 +16,9 @@ import { PassportModule } from '@nestjs/passport';
     TypeOrmModule.forFeature([User])
   ],
   controllers: [AuthController],
-  providers: [AuthService, UserRepository]
+  // JwtStrategy를 이 Auth 모듈에서 사용할 수 있게 등록
+  providers: [AuthService, UserRepository, JwtStrategy],
+  // JwtStrategy, PassportModule를 다른 모듈에서 사용할 수 있게 등록
+  exports: [JwtStrategy, PassportModule]
 })
 export class AuthModule {}
